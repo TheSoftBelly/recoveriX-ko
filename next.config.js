187b055daba89ab022c150f53d8cca1bd741f651
HyoJoon <jjoon102426@gmail.com>
2025-08-10 12:32:04 +0900

    test global scss

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
