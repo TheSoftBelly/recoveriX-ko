dc068d3e337e1d31c4923c3d5dd8ab7daebb151d
HyoJoon <jjoon102426@gmail.com>
2025-10-16 14:44:15 +0900

    fix: csp 설정

--- next.config.js
+++ next.config.js
@@ -12,13 +12,27 @@ const nextConfig = {
   },
 
   async headers() {
+    const cspHeader = `
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' https:;
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' blob: data: https:;
+      font-src 'self' data:;
+      connect-src 'self' https://akwisehgtrmytjhcilby.supabase.co wss://akwisehgtrmytjhcilby.supabase.co;
+      object-src 'none';
+      frame-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+    `.replace(/\s{2,}/g, ' ').trim();
+
     return [
       {
         source: '/:path*',
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https:;",
+            value: cspHeader,
           },
         ],
       },
